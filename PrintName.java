e8dc0a0f38672e648147321f15ee3e47f6bb68b2
Mayank4545 <keshri.mayank@gmail.com>
2016-11-20 16:55:18 +0530

    Assignment2.1
    print your name without using semicolon

---
+++ PrintName.java
@@ -0,0 +1,11 @@
+// Print name without using semicolon
+public class PrintName {
+public static void main(String[] args) {
+	
+	for (int i = 1; i < 2; i++) {
+		if (System.out.printf("Mayank Keshri")==null) {
+			
+		}		
+	}
+}
+}
